b48e7676a12be40dad899062e70539132e6ca4d9
ymamidi <ymamidi@uno.edu>
2021-01-27 16:31:15 -0600

    connecting to DB

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://sqlMG:Yashu!1996@cluster0.nudtm.mongodb.net/test');
+mongoose.connection.once('open',() => {
+    console.log('connected to database')
+});
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
