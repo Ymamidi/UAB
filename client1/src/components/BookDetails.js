5f3177d4ed4960e319f90195a887116b09c212fb
ymamidi <ymamidi@uno.edu>
2021-02-13 19:59:19 -0600

    made a single query

--- client1/src/components/BookDetails.js
+++ client1/src/components/BookDetails.js
@@ -3,16 +3,46 @@ import {graphql} from 'react-apollo';
 import {getBookQuery} from "../queries/queries";
 
 class BookDetails extends Component {
-    
+    displayBookDetails(){
+        const {book} = this.props.data;
+        if(book){
+            return(
+                <div>
+                    <h2>Book Name: {book.name}</h2>
+                    <p>Genre: {book.genre}</p>
+                    <p>Name: {book.author.name}</p>
+                    <p>Age: {book.author.age}</p>
+                    <p>All books by this author</p>
+                    <ul className="other-books">
+                        {book.author.books.map(item => {
+                            return <li key={item.id}>{item.name}</li>
+                        })}
+                    </ul>
+                </div>
+            ) 
+        }else {
+            return(
+                <div>No Books Selected</div>
+            )
+        }
+    }        
     render(){
       return (
         <div id= "book-details">
-            <p>output book details here</p>
+            {this.displayBookDetails()}
           
         </div>
       );
     }
 }
 
-export default graphql(getBookQuery)(BookDetails);
+export default graphql(getBookQuery, {
+    options:(props) => {
+        return {
+            variables: {
+                id: props.bookid
+            }
+        }
+    }
+})(BookDetails);
   
